699ddfe89d3484c1c9ef4e9e64385447d7bf1a2d
Andreas Thaler <a.thaler@thaler-software.de>
2018-03-24 21:39:05 +0100

    running soap web service example with server certificate

--- subsystems/fum/fum-it/src/test/java/de/thaso/fum/it/FumWebServiceHttpsIT.java
+++ subsystems/fum/fum-it/src/test/java/de/thaso/fum/it/FumWebServiceHttpsIT.java
@@ -8,9 +8,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.xml.ws.BindingProvider;
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
 import java.net.URL;
+import java.security.KeyStore;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -18,22 +24,50 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class FumWebServiceIT {
+public class FumWebServiceHttpsIT {
 
+    public static final String WEB_SERVICE_URL = "https://server:65401/fum/services/FumWS";
+    public static final String KEYSTORE_FILE = "/keystore.jks";
+    public static final String KEYSTORE_TYPE = "jks";
+    public static final String KEYSTORE_PASSWORD = "geheim";
+    public static final String WSDL_FILE = "/funWebServiceHttps.swdl";
     public static final String PING_TEXT = RandomStringUtils.randomAlphanumeric(10);
 
     private URL url;
-    private HttpURLConnection connection;
+    private HttpsURLConnection connection;
     private FumWS service;
     private FumWSPortType servicePort;
 
     @Before
     public void setUp() throws Exception {
-        url = new URL("http://localhost:65402/fum/services/FumWS?wsdl");
-//        url = new URL("http://localhost:8090/fum/services/FumWS?wsdl");
-        connection = (HttpURLConnection) url.openConnection();
+        //System.setProperty("javax.net.debug", "all");
+        url = new URL(WEB_SERVICE_URL +"?wsdl");
+        connection = (HttpsURLConnection) url.openConnection();
+        connection.setSSLSocketFactory(createSSLSocketFactory());
+
+        url = this.getClass().getResource(WSDL_FILE);
         service = new FumWS(url, FumWS.SERVICE);
         servicePort = service.getFumWSHttpSoap12Endpoint();
+
+        ((BindingProvider) servicePort).getRequestContext().put("com.sun.xml.ws.transport.https.client.SSLSocketFactory", createSSLSocketFactory());
+        ((BindingProvider) servicePort).getRequestContext().put("javax.xml.ws.service.endpoint.address", WEB_SERVICE_URL + ".FumWSHttpSoap12Endpoint/");
+    }
+
+    private SSLSocketFactory createSSLSocketFactory() throws Exception {
+        final TrustManagerFactory trustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(loadKeystore());
+        final SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
+        return sslContext.getSocketFactory();
+    }
+
+    private KeyStore loadKeystore() throws Exception {
+        final KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
+        final InputStream inputStream = this.getClass().getResourceAsStream(KEYSTORE_FILE);
+        keyStore.load(inputStream, KEYSTORE_PASSWORD.toCharArray());
+        inputStream.close();
+        return keyStore;
     }
 
     @Test
@@ -44,6 +78,15 @@ public class FumWebServiceIT {
         final int result = connection.getResponseCode();
         // then
         assertThat(result, is(200));
+
+/*
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line = bufferedReader.readLine();
+        while(line != null) {
+            System.out.println(line);
+            line = bufferedReader.readLine();
+        }
+*/
     }
 
     @Test
